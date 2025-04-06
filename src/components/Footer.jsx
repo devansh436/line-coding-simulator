d1aa65d03147e7d86b0e1880fdd74651bfd7b8a0
Devansh Deshpande <devansh436@gmail.com>
2025-04-06 21:49:15 +0530

    Footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
 function Footer() {
-    return (
-        <footer className="w-100 text-light text-center mt-auto" style={{fontSize : "14px", backgroundColor:"#333" }}>
-             &#169; Made by Devansh and Sahil
-        </footer>
-    )
+  return (
+    <footer
+      className="w-100 text-light text-center mt-auto py-2"
+      style={{
+        fontSize: "14px",
+        backgroundColor: "#1d1d1d",
+        borderTop: "1px solid #444",
+      }}
+    >
+      &#169; Made with <span style={{ color: "#ff4d6d" }}>&hearts;</span> by
+      <br />
+      <strong>Devansh Deshpande</strong> (23BCE056) <br />
+      <strong>Sahil Chandel</strong> (23BCE041)
+    </footer>
+  );
 }
 
 export default Footer;
