5e39d9ea7c2a6e922077468dee603341489d6edb
Devansh Deshpande <devansh436@gmail.com>
2025-04-06 21:36:22 +0530

    Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Simulation from './pages/Simulation';
-import Footer from './components/Footer';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Simulation from "./pages/Simulation";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="d-flex flex-column bg-dark min-vh-100">
     <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}>  </Route>
-        <Route path="/simulation" element={<Simulation/>}>  </Route>
-      </Routes>
+      <div className="d-flex flex-column bg-dark min-vh-100">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/simulation" element={<Simulation />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
-      <Footer/>
-    </div>
-  )
+  );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/line-coding-simulator/',
+  base: "/line-coding-simulator/",
   plugins: [react()],
 })
